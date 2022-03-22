938d6fdb3bf7cab8ba290853554360fa59441bf3
Taras Ishchuk <webforyou@email.cz>
2022-03-22 21:55:03 +0100

    smazal všechno do háje, protože napsal úplné kraviny

--- server.js
+++
@@ -1,8 +0,0 @@
-// Vyžaduje HTTP jako moduly serveru WebSocket
-var express = require("express");
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-// Poskytujte statická aktivy ze složky „public“.
-app.use("/", express.static('public'));
